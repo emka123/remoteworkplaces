b7597689fef0385ec53f99009d60470b6b169cb1
madzik <magdalena.pal90@gmail.com>
2015-09-25 22:44:12 +0200

    Add migrations

---
+++ digitalnomads/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Place',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('image', models.ImageField(upload_to='static/photos', verbose_name='Photo')),
+            ],
+        ),
+    ]
