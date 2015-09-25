6761194cd30e6cc6d75bf3903126d781d7cddd82
madzik <magdalena.pal90@gmail.com>
2015-09-25 21:48:31 +0200

    Add models

---
+++ digitalnomads/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+class Place(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    image = models.ImageField('Photo', upload_to='static/photos')
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
