4c6a9734d3fdd7fa182aedff15596d69b30f5bee
madzik <magdalena.pal90@gmail.com>
2015-10-08 19:14:49 +0200

    Add link for new site 'About me'

--- digitalnomads/urls.py
+++ digitalnomads/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from . import views
 urlpatterns = [
     url(r'^$', views.base),
-    url(r'^city', views.place_list)
-
+    url(r'^city', views.place_list),
+    url(r'^about_me', views.about_me), 
 ]
