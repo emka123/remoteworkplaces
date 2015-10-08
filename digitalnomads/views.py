a09d878beafdd827a323eabd2ab30d0ee2e2cf19
madzik <magdalena.pal90@gmail.com>
2015-10-08 19:12:04 +0200

    Add path for 'about_me'

--- digitalnomads/views.py
+++ digitalnomads/views.py
@@ -6,7 +6,8 @@ def place_list(request):
     places = Place.objects.order_by('title')
     return render(request, 'digitalnomads/place_list.html', {'places': places})
 
-
-
 def base(request):
     return render(request, 'digitalnomads/base.html')
+
+def about_me(request):
+	return render(request, 'digitalnomads/about_me.html')
