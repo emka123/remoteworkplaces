77c1bb792ec5ecda1faa04e3717e438ad3e2f8de
madzik <magdalena.pal90@gmail.com>
2015-10-14 19:43:43 +0200

    Change conception - add 'about site' for base view

--- digitalnomads/views.py
+++ digitalnomads/views.py
@@ -2,12 +2,9 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Place
 
-def place_list(request):
-    places = Place.objects.order_by('title')
-    return render(request, 'digitalnomads/place_list.html', {'places': places})
-
 def base(request):
-    return render(request, 'digitalnomads/base.html')
+    places = Place.objects.order_by('title')
+    return render(request, 'digitalnomads/base.html', {'places': places})
 
 def about_me(request):
 	return render(request, 'digitalnomads/about_me.html')
