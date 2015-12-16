d5e6487145d4b1a83165f38594eed0165a2434c0
madzik <magdalena.pal90@gmail.com>
2015-12-16 22:40:09 +0100

    Change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,15 +75,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-if ON_HEROKU == '1':
-import dj_database_url
-DATABASES = {'default': dj_database_url.config()}
-else:
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.sqlite3',
-'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Internationalization
