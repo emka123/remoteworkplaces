12f50d0356435472b0d6f0c25c943404f74119d4
madzik <magdalena.pal90@gmail.com>
2016-03-09 18:29:53 +0100

    Refactoring

--- static/custom.js
+++ static/custom.js
@@ -1,22 +1,10 @@
-
-jQuery(function () {
-	"use strict";
-    
-    /*global jQuery $*/ 
-	//Slider 
-	$(document).ready(function(){
-		
-		jQuery('.skillbar').each(function() {
-			jQuery(this).appear(function() {
-				jQuery(this).find('.count-bar').animate({
-					width:jQuery(this).attr('data-percent')
-				},3000);
-				var percent = jQuery(this).attr('data-percent');
-				jQuery(this).find('.count').html('<span>' + percent + '</span>');
-			});
-		});	
-	});
-	
-}());
-
- 
+ $(document).ready(function(){
+ 	 $('a[href^="#"]').click(function() {
+        var hash = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - 50
+        }, 500);
+        return false;
+    });
+ });
+	 
